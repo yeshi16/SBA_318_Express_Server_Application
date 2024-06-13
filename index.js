140c0f89d0635ac2f1c92d37de6dcac6044c5031
m-s1621 <mebashimelis@gmail.com>
2024-06-13 16:47:56 -0400

    pug templet gengin

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const app = express()
 const port = 3000;
 const path = require('path')
 
-const notes = require('./routes/notes')
+const notesRouter = require('./routes/notes')
 
 // middleware for parsing json
 app.use(express.json())
 
+// set template engine
+app.set('view engine', 'pug'); // use pug view engine 
+app.set('views', path.join(__dirname, 'views'))
+
 // notes route
-app.use('/api/notes', notes)
+app.use('/notes', notesRouter)
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}.`)
 })
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    // res.send("Hello World")
+    res.render('home')
 })
+
+app.js
--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+const notes = [{id: 1, title: 'first note', content: 'first content'}, {title: 'second note', content: 'second content'}]
+
 // get all notes
-router.get('/', (req, res) => {
-    res.send('you can find all notes here')
+router.get('/', (req, res) => { //  :title/:content
+    // res.send('you can find all notes here')
+    res.render('notes', {notes}) // {title: req.params.title, content: req.params.content}
 })
 
 // add new note
 router.post('/', (req, res) => {
-    res.send('you can post new note here')
+    // res.send('you can post new note here')
+    const note = {id: notes.length + 1, title: req.body.title, content: req.body.content}
+    notes.push(note)
+    res.redirect('/notes')
 })
 
 // update note
@@ -22,3 +28,5 @@ router.delete('/:id', (req, res) => {
 })
 
 module.exports = router
+
+
