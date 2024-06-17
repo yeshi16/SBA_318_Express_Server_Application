64fe129cb54a6c7da01c0bcf3dbbe358eb65a1a6
m-s1621 <mebashimelis@gmail.com>
2024-06-16 23:25:20 -0400

    seperated data

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const notes = [{id: 1, posted: new Date().toLocaleTimeString(), title: 'What is Lorem Ipsum?', content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'}] //[{id: 1, title: 'first note', content: 'first content'}, {title: 'second note', content: 'second content'}]
+const notes = require('../data/notes')  // [{id: 1, posted: new Date().toLocaleTimeString(), title: 'What is Lorem Ipsum?', content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'}] //[{id: 1, title: 'first note', content: 'first content'}, {title: 'second note', content: 'second content'}]
 
 // get all notes
 router.get('/', (req, res) => { //  :title/:content
